12e6fc9d78b0b2d2aaccca56319ee6c5a7ebf2b0
IamDipanda <aserdipanda@gmail.com>
2021-01-21 17:00:18 +0100

    Add search commits

--- search.go
+++ search.go
@@ -6,7 +6,6 @@ import (
 	"github.com/PuerkitoBio/goquery"
 	"math"
 	"net/http"
-	"net/url"
 )
 
 const (
@@ -24,6 +23,16 @@ type Repository struct {
 	ProgrammingLanguage string
 	UpdateTime          string
 	Error               error
+}
+
+type Commit struct {
+	RepositoryName string
+	RepositoryLink string
+	CommitLink     string
+	CommitMessage  string
+	Author         string
+	CommitDate     string
+	Error          error
 }
 
 type searchMode string
@@ -45,9 +54,9 @@ func buildSearchUrl(query string, typee searchMode, opt sortOptions) string {
 
 	switch typee {
 	case searchModeMarketPlace, searchModeTopics:
-		return githubBaseUrl + url.QueryEscape(fmt.Sprintf("/search?q=%s&type=%s", query, typee))
+		return githubBaseUrl + fmt.Sprintf("/search?q=%s&type=%s", query, typee)
 	default:
-		return githubBaseUrl + url.QueryEscape(fmt.Sprintf("/search?o=%s&q=%s&s=%s&type=%s", opt.getOrder(), query, opt.name, typee))
+		return githubBaseUrl + fmt.Sprintf("/search?o=%s&q=%s&s=%s&type=%s", opt.getOrder(), query, opt.name, typee)
 	}
 }
 
@@ -62,7 +71,7 @@ func getMaxPage(maxResult int) int {
 	return int(math.Round(float64(maxResult) / float64(resultPerPage)))
 }
 
-// SearchRepositories returns channel with repositories for a given search query
+// SearchRepositories returns channel with Repository for a given search query
 func (s *Scraper) SearchRepositories(opt sortOptions, query string, maxResult int) <-chan *Repository {
 	channel := make(chan *Repository)
 
@@ -113,4 +122,53 @@ func (s *Scraper) SearchRepositories(opt sortOptions, query string, maxResult in
 // SearchRepositories wrapper for default Scraper
 func SearchRepositories(opt sortOptions, query string, maxResult int) <-chan *Repository {
 	return defaultScraper.SearchRepositories(opt, query, maxResult)
+}
+
+// SearchCommits returns channel with Commit for a given search query
+func (s *Scraper) SearchCommits(opt sortOptions, query string, maxResult int) <-chan *Commit {
+	channel := make(chan *Commit)
+
+	go func() {
+		defer close(channel)
+		url := buildSearchUrl(query, searchModeCommits, opt)
+		maxpage := getMaxPage(maxResult)
+		for page := 1; page <= maxpage; page++ {
+			res, err := s.client.Get(url + fmt.Sprintf("&p=%v", page))
+			if err != nil {
+				channel <- &Commit{Error: err}
+				return
+			}
+
+			if !successfulLoaded(res) {
+				channel <- &Commit{Error: errors.New("Failed to load page")}
+				return
+			}
+
+			doc, err := goquery.NewDocumentFromReader(res.Body)
+			if err != nil {
+				channel <- &Commit{Error: errors.New("Failed to read page")}
+				return
+			}
+
+			doc.Find("div.commits-list-item > div.mt-n1").Each(func(i int, selection *goquery.Selection) {
+				var commit Commit
+				commit.RepositoryName, _ = selection.Find("a.link-gray").Attr("href")
+				commit.RepositoryLink = githubBaseUrl + commit.RepositoryName
+				commit.CommitMessage = selection.Find("div.f4 > a.message").Text()
+				clink, _ := selection.Find("div.f4 > a.message").Attr("href")
+				commit.CommitLink = githubBaseUrl + clink
+				commit.Author = selection.Find("a.commit-author").Text()
+				commit.CommitDate, _ = selection.Find("relative-time").Attr("datetime")
+
+				channel <- &commit
+			})
+			res.Body.Close()
+		}
+	}()
+	return channel
+}
+
+//SearchCommits wrapper for default Scraper
+func SearchCommits(opt sortOptions, query string, maxResult int) <-chan *Commit {
+	return defaultScraper.SearchCommits(opt, query, maxResult)
 }
