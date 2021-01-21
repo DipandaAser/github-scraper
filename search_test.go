bbb901a44a53c17bae4e487c6e2605c3e3f5d410
IamDipanda <aserdipanda@gmail.com>
2021-01-21 20:28:51 +0100

    Add test for SearchIssues

--- search_test.go
+++ search_test.go
@@ -7,11 +7,11 @@ import (
 func TestScraper_SearchRepositories(t *testing.T) {
 	t.Run("Test SearchRepositories", func(t *testing.T) {
 		s := New()
-		repoCount := 0
+		count := 0
 		for _ = range s.SearchRepositories(DefaultSortOption, "go", 20) {
-			repoCount++
+			count++
 		}
-		if repoCount < 20 {
+		if count < 20 {
 			t.Fatal("SearchRepositories faild. Can't get 20 items.")
 		}
 	})
@@ -20,12 +20,25 @@ func TestScraper_SearchRepositories(t *testing.T) {
 func TestScraper_SearchCommits(t *testing.T) {
 	t.Run("Test SearchCommits", func(t *testing.T) {
 		s := New()
-		repoCount := 0
+		count := 0
 		for _ = range s.SearchCommits(DefaultSortOption, "go", 20) {
-			repoCount++
+			count++
 		}
-		if repoCount < 20 {
+		if count < 20 {
 			t.Fatal("SearchCommits faild. Can't get 20 items.")
+		}
+	})
+}
+
+func TestScraper_SearchIssues(t *testing.T) {
+	t.Run("Test SearchIssues", func(t *testing.T) {
+		s := New()
+		count := 0
+		for _ = range s.SearchIssues(DefaultSortOption, "go", 20) {
+			count++
+		}
+		if count < 20 {
+			t.Fatal("SearchIssues faild. Can't get 20 items.")
 		}
 	})
 }
